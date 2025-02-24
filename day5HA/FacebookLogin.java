7934b421a0f4b1e7348aadeaf1502e36795661fd
Atharsh-01 <atharshsnraj@gmail.com>
2025-02-24 21:39:10 +0530

    Add files via upload

---
+++ day5HA/FacebookLogin.java
@@ -0,0 +1,23 @@
+package day5HA;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookLogin {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+		driver.findElement(By.id("email")).sendKeys("testleaf.2023@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("Tuna@321");
+		driver.findElement(By.name("login")).click();
+		
+		driver.findElement(By.linkText("Find your account and log in.")).click();
+		String title = driver.getTitle();
+		System.out.println(title);
+	}
+
+}
